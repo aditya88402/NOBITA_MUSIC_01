0c21fa241220f38808cb43a8772e169bb145327a
iamnobita01 <rekhachauhan206243@gmail.com>
2025-07-12 16:02:39 +0530

    𝚴 𝐎 𝐁 𝚰 𝐓 𝚲

--- config.py
+++ config.py
@@ -44,15 +44,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shaurya-0829/NOBITA_MUSIC",
+    "https://github.com/iamnobita01/NOBITA_MUSIC_01",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/megaverse0008")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+8GBKKJX-vvUyMzhl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NOBITA_MUSIC_SUPPORT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+wPjAlUcObehiZDM1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
